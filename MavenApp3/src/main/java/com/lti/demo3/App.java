cf58014b432aa0aa9d3865662dc17613242053f4
nitishr247 <nitishrajgor@gmail.com>
2021-07-29 16:31:52 +0530

    First Commit

---
+++ MavenApp3/src/main/java/com/lti/demo3/App.java
@@ -0,0 +1,8 @@
+package com.lti.demo3;
+
+public class App {
+	int add(int a,int b) {
+		return a+b;
+	}
+
+}
--- MavenApp3/src/test/java/com/lti/demo3/AppTest.java
+++ MavenApp3/src/test/java/com/lti/demo3/AppTest.java
@@ -1,22 +1,24 @@
-package com.lti.demo;
+package com.lti.demo3;
 
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class CalculatorTest {
+public class AppTest {
 
 	@Test
 	public void addtest1() {
-		int res=new Calculator().add(10, 20);
+		int res=new App().add(10, 20);
 		assertEquals(30, res);
 	}
 	
 	@Test
 	public void addtest2() {
-		int res=new Calculator().add(30, 20);
+		int res=new App().add(30, 20);
 		assertEquals(50, res);
 	}
 
